783b4c87db433095428be77bd5c24936adec180b
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-11-14 16:34:00 +0900

    add sample line bot

--- pages/api/bot/webhook.ts
+++ pages/api/bot/webhook.ts
@@ -1,7 +1,17 @@
+import { middleware, validateSignature } from "@line/bot-sdk"
 import { NextApiRequest, NextApiResponse } from "next"
+import client from "../../../utils/linebot/initLinebot"
+import runMiddleware from "../../../utils/router/runMiddleware"
 
 const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200)
+  await runMiddleware(req, res, middleware)
+  // const signature = req.headers["X-Line-Signature"].toString()
+  // validateSignature(req.body, process.env.LINE_BOT_CHANNEL_SECRET, signature)
+  
+  return client.replyMessage(req.body.replyToken, {
+    type: 'text',
+    text: req.body.messages[0].text
+  });
 }
 
 export default webhook
---
+++ utils/linebot/initLinebot.ts
@@ -0,0 +1,10 @@
+import { Client, middleware as lineMiddleware } from '@line/bot-sdk';
+
+const config = {
+  channelAccessToken: process.env.LINE_BOT_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_BOT_CHANNEL_SECRET
+}
+const client = new Client(config);
+export default client
+
+export const middleware = lineMiddleware(config)
