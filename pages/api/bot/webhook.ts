7e587d691c5ca90e8c8fd9bfb065787ab8a0c71f
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-11-14 16:57:56 +0900

    add reply token

--- pages/api/bot/webhook.ts
+++ pages/api/bot/webhook.ts
@@ -6,11 +6,17 @@ const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, linebotMiddleware)
   // const signature = req.headers["X-Line-Signature"].toString()
   // validateSignature(req.body, process.env.LINE_BOT_CHANNEL_SECRET, signature)
+
+  const replyToken = req.body.events[0].replyToken
   
-  return client.replyMessage(req.body.replyToken, {
+  const message = await client.replyMessage(req.body.replyToken, {
     type: 'text',
     text: req.body.messages[0].text
-  });
+  })
+  return res.status(200).json({
+    replyToken,
+    messages: [message],
+  })
 }
 
 export default webhook
